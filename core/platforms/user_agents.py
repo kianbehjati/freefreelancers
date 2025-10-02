1561aa9b59445abf91752060f619e9a223000200
Kian <kianbehjati_84@outlook.com>
2025-10-02 21:54:01 +0330

    User Agents File

---
+++ core/platforms/user_agents.py
@@ -0,0 +1,6 @@
+UserAgents = {
+    "1" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36", #pc
+    "2" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0", #pc
+    "3" : "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1", #Iphone
+    "4" : "Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Mobile Safari/537.36" #Android
+}
